6c7c6e53576735c516d8739996d5377adc1d2f60
Mariano Guerra <mariano@marianoguerra.org>
2020-02-23 15:51:00 +0100

    packaging config, some config renames

--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule Rcl.MixProject do
   def project do
     [
       app: :rcl,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -20,6 +22,23 @@ defmodule Rcl.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    """
+    Mix task to setup a Riak Core Lite project 
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["Mariano Guerra"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/riak-core-lite/mix_riak_core_lite_task"},
+      files: ["lib", "mix.exs", "README.md", ".formatter.exs", "templates"]
+    ]
   end
 end
