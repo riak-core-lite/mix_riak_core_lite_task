0b09e218d7b18e1d2d7c5a8584bfe7a80b26f70b
Mariano Guerra <mariano@marianoguerra.org>
2020-09-27 15:40:49 +0200

    remove redundant config from env config files, add ct env config

--- templates/basic/config/node1.exs
+++ templates/basic/config/node1.exs
@@ -4,5 +4,4 @@ use Mix.Config
 config :riak_core,
   ring_state_dir: 'data/node1/data_riak_core',
   platform_data_dir: 'data/node1/data_riak_core',
-  ring_creation_size: 64,
   handoff_port: 8199
--- templates/basic/config/node3.exs
+++ templates/basic/config/node3.exs
@@ -4,5 +4,4 @@ use Mix.Config
 config :riak_core,
   ring_state_dir: 'data/node3/data_riak_core',
   platform_data_dir: 'data/node3/data_riak_core',
-  ring_creation_size: 64,
   handoff_port: 8399
